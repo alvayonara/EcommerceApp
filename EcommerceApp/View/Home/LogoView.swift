2f67ef7819336b7e1f018706709579602e1e37a6
Alva Yonara Puramandya <alvayonara@outlook.com>
2021-02-20 11:11:09 +0700

    Create custom navigation bar

---
+++ EcommerceApp/View/Home/LogoView.swift
@@ -0,0 +1,41 @@
+//
+//  LogoView.swift
+//  EcommerceApp
+//
+//  Created by Alva Yonara Puramandya on 20/02/21.
+//
+
+import SwiftUI
+
+struct LogoView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        HStack(spacing: 4) {
+            Text("Touch".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+            
+            Image("logo-dark")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30, height: 30, alignment: .center)
+            
+            Text("Down".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+        }
+    }
+}
+
+// MARK: - PREVIEW
+struct LogoView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogoView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
