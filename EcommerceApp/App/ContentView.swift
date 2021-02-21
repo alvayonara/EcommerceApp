f2fbe4c3c9bfa53b2809008c7e5090bf21df0b53
Alva Yonara Puramandya <alvayonara@outlook.com>
2021-02-21 14:53:24 +0700

    Create product section using vertical grid view

--- EcommerceApp/App/ContentView.swift
+++ EcommerceApp/App/ContentView.swift
@@ -29,6 +29,15 @@ struct ContentView: View {
                         
                         CategoryGridView()
                         
+                        TitleView(title: "Helmet")
+                        
+                        LazyVGrid(columns: gridLayout, spacing: 15, content: {
+                            ForEach(products) { product in
+                                ProductItemView(product: product)
+                            }
+                        })
+                        .padding(15)
+                        
                         FooterView()
                             .padding(.horizontal)
                     }
