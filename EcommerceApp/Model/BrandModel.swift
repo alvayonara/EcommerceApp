6b86d7516857a33400975218d3aaa9e8dec68625
Alva Yonara Puramandya <alvayonara@outlook.com>
2021-02-21 15:15:12 +0700

    Create brands section using horizontal grid view

---
+++ EcommerceApp/Model/BrandModel.swift
@@ -0,0 +1,13 @@
+//
+//  BrandModel.swift
+//  EcommerceApp
+//
+//  Created by Alva Yonara Puramandya on 21/02/21.
+//
+
+import Foundation
+
+struct Brand: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
